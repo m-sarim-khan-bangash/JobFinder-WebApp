d81f4eab5d5f6f335a50887bd0045864ab58c1cf
Muhammad Sarim Khan Bangash <75114586+m-sarim-khan-bangash@users.noreply.github.com>
2024-05-13 16:22:42 +0500

    JobFinder WebApp

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import React from "react";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
+import NotFound from "./pages/NotFound";
+import JobPage from "./pages/JobPage";
+import AddJobPage from "./pages/AddJobPage";
+import EditJobPage from "./pages/EditJobPage";
+
+const App = () => {
+  // Add a new job
+  const addJob = async (newJob) => {
+    const res = await fetch("http://localhost:8000/jobs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+    // console.log(newJob);
+  };
+
+  // Delete a job
+  const deleteJob = async (id) => {
+    const res = await fetch(`http://localhost:8000/jobs/${id}`, {
+      method: "DELETE",
+    });
+    return;
+  };
+
+  // Edit a Job
+  
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/jobs" element={<JobsPage />} />
+        <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route path="/jobs/:id" element={<JobPage deleteJob={deleteJob} />} />
+        <Route path="/edit-job/:id" element={<EditJobPage />}/>
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    )
+  );
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/JobListings.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import JobListing from "./JobListing";
+
+const JobListings = ({ isHome = false }) => {
+  // console.log(jobs);
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const apiUrl = isHome ? "http://localhost:8000/jobs?_limit=3" : "http://localhost:8000/jobs";
+
+      try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        setJobs(data);
+        setLoading(false);
+      } catch {
+        console.log("Error fetching data", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchJobs();
+  }, []);
+
+  return (
+    <div>
+      <section className="bg-blue-50 px-4 py-10">
+        <div className="container-xl lg:container m-auto">
+          <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+            {isHome ? "Recent Jobs" : "Browse Jobs"}
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {loading ? (
+              <p>Loading...</p>
+            ) : (
+              <>
+                {jobs.map((job, index) => {
+                  return <JobListing key={index} job={job} />;
+                })}
+              </>
+            )}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default JobListings;
